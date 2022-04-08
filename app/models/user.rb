14d6d442dc5e67f0b14ac81c5360b3d54932db03
Nikola <woleet@outlook.com>
2022-04-08 04:51:40 +0200

    Active Storage to Devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ApplicationRecord
   scope :all_except, -> (user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
   has_many :messages
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_user_crop.png')),
+      filename: 'default_user_crop.png',
+      content_type: 'image/png'
+    )
+  end
 end
